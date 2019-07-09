9f9417016f29f27256b6f1ecab68ebbb02241325
Olivia Cunningham <Liv.cunninghamgb@gmail.com>
2019-07-09 17:18:21 -0400

    things are better: mapping, building database done

--- ClientApp/src/Pages/Admin.jsx
+++ ClientApp/src/Pages/Admin.jsx
@@ -1,28 +1,244 @@
-import React, { useState, useEffect } from 'react'
+import React, { Component } from 'react'
+import AddressSuggest from '../components/AddressSuggest.jsx'
+import AddressInput from '../components/AddressInput.jsx'
 import axios from 'axios'
 
-export default function Admin() {
-  const [Location, setLocation] = useState('')
+const APP_ID_HERE = 'AMsXJCY7VtRdqUqhD4Nr'
+const APP_CODE_HERE = 'bAfnOReqDXdAlzKbbz6mCA'
 
-  //API
-  //Beginning with Geolocation
-  const getLocation = e => {
-    e.preventDefault()
+class Admin extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = this.getInitialState()
+    // this.state = this.getPlaces()
+
+    // Admin has entered something in the address bar
+    this.onQuery = this.onQuery.bind(this)
+    // Admin has entered something in an address field
+    this.onAddressChange = this.onAddressChange.bind(this)
+    // Admin has clicked the check button
+
+    // Admin has clicked the clear button
+    this.onClear = this.onClear.bind(this)
+  }
+
+  onQuery(evt) {
+    const query = evt.target.value
+
+    if (!query.length > 0) {
+      this.setState(this.getInitialState())
+      return
+    }
+    //AUTOCOMPLETE
+    const self = this
     axios
-      .get(
-        'https://places.demo.api.here.com/places/v1/discover/explore?app_id=AMsXJCY7VtRdqUqhD4Nr&app_code=bAfnOReqDXdAlzKbbz6mCA'
-      )
-      .then(resp => {
-        console.log({ resp }).get('')
+      .get('https://autocomplete.geocoder.api.here.com/6.2/suggest.json', {
+        params: {
+          app_id: APP_ID_HERE,
+          app_code: APP_CODE_HERE,
+          query: query,
+          maxresults: 1
+        }
+      })
+      .then(function(response) {
+        if (response.data.suggestions.length > 0) {
+          const id = response.data.suggestions[0].locationId
+          const address = response.data.suggestions[0].address
+          self.setState({
+            address: address,
+            query: query,
+            locationId: id
+          })
+        } else {
+          const state = self.getInitialState()
+          self.setState(state)
+        }
       })
   }
 
-  return (
-    <>
-      <form onSubmit={getLocation}>
-        <input type="text" placeholder="Add-A-Place-Bitch" />
-        <button>+</button>
-      </form>
-    </>
-  )
+  getInitialState() {
+    return {
+      results: [],
+      address: {
+        street: '',
+        city: '',
+        state: '',
+        postalCode: '',
+        country: ''
+      },
+      query: '',
+      locationId: '',
+      isChecked: false,
+      coords: {}
+    }
+  }
+
+  getPlaces() {
+    return {
+      results: {
+        title: '',
+        averageRating: '',
+        tags: '',
+        openingHours: '',
+        isOpen: false,
+        category: '',
+        coords: {}
+      }
+    }
+  }
+
+  onClear = evt => {
+    const state = this.getInitialState()
+    this.setState(state)
+  }
+
+  onAddressChange = evt => {
+    const id = evt.target.id
+    const val = evt.target.value
+
+    let state = this.state
+    state.address[id] = val
+    this.setState(state)
+  }
+
+  onCheck = evt => {
+    let params = {
+      app_id: APP_ID_HERE,
+      app_code: APP_CODE_HERE
+    }
+
+    if (this.state.locationId.length > 0) {
+      params['locationId'] = this.state.locationId
+    } else {
+      params['searchtext'] =
+        this.state.address.street +
+        this.state.address.city +
+        this.state.address.state +
+        this.state.address.postalCode +
+        this.state.address.country
+    }
+
+    //GEOCODER
+    axios
+      .get('https://geocoder.api.here.com/6.2/geocode.json', { params: params })
+      .then(response => {
+        const view = response.data.Response.View
+        if (view.length > 0 && view[0].Result.length > 0) {
+          const location = view[0].Result[0].Location
+
+          this.setState(
+            {
+              isChecked: 'true',
+              locationId: '',
+              query: location.Address.Label,
+              address: {
+                street:
+                  location.Address.HouseNumber + ' ' + location.Address.Street,
+                city: location.Address.City,
+                state: location.Address.State,
+                postalCode: location.Address.PostalCode,
+                country: location.Address.Country
+              },
+              coords: {
+                lat: location.DisplayPosition.Latitude,
+                lon: location.DisplayPosition.Longitude
+              }
+            },
+            () => {
+              axios
+                .get(
+                  `https://places.cit.api.here.com/places/v1/discover/search?q=restaurant&app_id=AMsXJCY7VtRdqUqhD4Nr&app_code=bAfnOReqDXdAlzKbbz6mCA&at=${
+                    this.state.coords.lat
+                  },${this.state.coords.lon}`
+                )
+                .then(resp => {
+                  console.log('ran')
+                  console.log({ resp })
+                  this.setState({
+                    results: resp.data.results.items
+                  })
+                })
+            }
+          )
+        } else {
+          this.setState({
+            isChecked: true,
+            coords: null
+          })
+        }
+      })
+      .catch(error => {
+        console.log('caught failed query')
+        this.setState({
+          isChecked: true,
+          coords: null
+        })
+      })
+  }
+
+  alert() {
+    if (!this.state.isChecked) {
+      return
+    }
+
+    if (this.state.coords === null) {
+      return (
+        <div className="alert alert-warning" role="alert">
+          <b>Invalid.</b> The address is not recognized.
+        </div>
+      )
+    } else {
+      return (
+        <div className="alert alert-success" role="alert">
+          <b>Valid Address.</b> Location is {this.state.coords.lat},{' '}
+          {this.state.coords.lon}.
+        </div>
+      )
+    }
+  }
+
+  render() {
+    let outcome = this.alert()
+    return (
+      <div className="container">
+        <AddressSuggest query={this.state.query} onChange={this.onQuery} />
+        <AddressInput
+          city={this.state.address.city}
+          state={this.state.address.state}
+          postalCode={this.state.address.postalCode}
+          country={this.state.address.country}
+          onChange={this.onAddressChange}
+        />
+        <br />
+        {outcome}
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={this.onCheck}
+        >
+          Check
+        </button>
+        <button
+          type="submit"
+          className="btn btn-outline-secondary"
+          onClick={this.onClear}
+        >
+          Clear
+        </button>
+        <ul className="answer-list">
+          {this.state.results.map(index => {
+            return (
+              <li key={index}>
+                <p>{index.items}</p>
+              </li>
+            )
+          })}
+          ``
+        </ul>
+      </div>
+    )
+  }
 }
+
+export default Admin
--- ClientApp/src/Pages/NewDate.jsx
+++ ClientApp/src/Pages/NewDate.jsx
@@ -1,24 +1,8 @@
-// import React from 'react'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+// import React, { useState } from 'react'
 // import axios from 'axios'
 import './NewDate.scss'
 import { Link } from 'react-router-dom'
-
-// //API
-// //Beginning with Geolocation
-// const getLocation = e => {
-//   e.preventDefault()
-//   axios
-//     .get(
-//       'https://places.demo.api.here.com/places/v1/discover/explore?app_id=AMsXJCY7VtRdqUqhD4Nr&app_code=bAfnOReqDXdAlzKbbz6mCA'
-//     )
-//     .then(resp => {
-//       console.log({ resp })
-//       .get(
-//       ''
-//       )
-//     })
-// }
 
 export default function NewDate() {
   return (
